cb255420de71ae3267ddb98fde80c1bf4728349e
Jeremy Watt <jwatt37@hotmail.com>
2018-03-20 08:00:11 +0900

    Update onDeleteSecret.js

--- functions/2fa/onDeleteSecret.js
+++ functions/2fa/onDeleteSecret.js
@@ -1,6 +1,5 @@
 const functions = require('firebase-functions');
 
-// The Firebase Admin SDK to access the Firebase Realtime Database. 
 const admin = require('firebase-admin');
 try {admin.initializeApp(functions.config().database);} catch(e) {}
 
